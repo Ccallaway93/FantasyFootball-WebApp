18a779d18378124a21d0efb56a2b39c519f373df
Conner Callaway <Ccallaway93@gmail.com>
2020-09-02 17:21:04 -0500

    renamed the headTohead js file in src

--- src/routes/headTohead.js
+++ src/routes/headTohead.js
@@ -8,61 +8,60 @@ const TeamRecords = require('../models/teamRecords')
 
 router.get('/headTohead', (req, res) => {
 
-    res.render('headTohead');
 
-    // TeamRecords.find({'year': 'Total', 'name': 'Erik'}).then((data) => {
-    //     console.log(data);
-    //     res.render('headTohead',data);
-    // })
+    TeamRecords.find({'year': 'Total', 'name': 'Erik'}).then((data) => {
+        console.log(data);
+        res.render('headTohead',data);
+    })
 
 })
 
 router.post('/headTohead', (req, res) => {
-    //  let total = []
+     let total = []
 
-    // const left = TeamRecords.find({'year': 'Total', 'team_id': req.body.leftId}).then((data) => {
-    //     let Lefttotal = data[0].h2h;
-    //     return _.find(Lefttotal, {'oppId': parseInt(req.body.rightId)});
-    // })
-    //  total.push(left);
+    const left = TeamRecords.find({'year': 'Total', 'team_id': req.body.leftId}).then((data) => {
+        let Lefttotal = data[0].h2h;
+        return _.find(Lefttotal, {'oppId': parseInt(req.body.rightId)});
+    })
+     total.push(left);
 
-    //  const right = TeamRecords.find({'year': 'Total', 'team_id': req.body.rightId}).then((data) => {
-    //     let RightTotal = data[0].h2h;
-    //     return _.find(RightTotal, {'oppId': parseInt(req.body.leftId)});
-    // })
-    //  total.push(right)
-
-
-    //     const record = TeamRecords.find({'team_id': req.body.leftId}).then((data) => {
-    //     let matchups = _.filter(data, function(o) {return o.year !== 'Total' && o.year !== '2017'})
-    //     let games = [];
-
-    //     for(var i = 0; i < matchups.length; i++) {
-    //         _.forEach(matchups[i].h2h, (game) => {
-    //             //console.log(game);
-    //             if(game.oppId === parseInt(req.body.rightId)){
-    //                 games.push(game);
-    //             }
-    //         })
-    //     }
-    //     return games
-    //  })
-    //  total.push(record);
+     const right = TeamRecords.find({'year': 'Total', 'team_id': req.body.rightId}).then((data) => {
+        let RightTotal = data[0].h2h;
+        return _.find(RightTotal, {'oppId': parseInt(req.body.leftId)});
+    })
+     total.push(right)
 
 
+        const record = TeamRecords.find({'team_id': req.body.leftId}).then((data) => {
+        let matchups = _.filter(data, function(o) {return o.year !== 'Total' && o.year !== '2017'})
+        let games = [];
+
+        for(var i = 0; i < matchups.length; i++) {
+            _.forEach(matchups[i].h2h, (game) => {
+                //console.log(game);
+                if(game.oppId === parseInt(req.body.rightId)){
+                    games.push(game);
+                }
+            })
+        }
+        return games
+     })
+     total.push(record);
 
 
 
-    //  return Promise.all(total).then((results) => {
-    //      const data = {
-    //          left: results[0],
-    //          right: results[1],
-    //          games: results[2]
-    //      };
-    //      res.send(data);
-    //  }).catch((e) => {
-    //      console.log(e)
-    //  })
+
+
+     return Promise.all(total).then((results) => {
+         const data = {
+             left: results[0],
+             right: results[1],
+             games: results[2]
+         };
+         res.send(data);
+     }).catch((e) => {
+         console.log(e)
+     })
 
 })
 
